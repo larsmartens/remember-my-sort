eed058a16ef4037a6d740b463f9a05623d74e6f1
hxreborn <hxreborn@duck.com>
2026-03-18 09:33:50 +0100

    chore: upgrade build stack to Gradle 9.4.0, AGP 9.1.0, Kotlin 2.3.20

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,20 +7,12 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
         google()
         mavenCentral()
-        mavenLocal {
-            content {
-                includeGroup("io.github.libxposed")
-            }
-        }
-    }
-    versionCatalogs {
-        create("libs")
     }
 }
 
 rootProject.name = "RememberMySort"
-
 include(":app")
